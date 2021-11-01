49dd07a172720fc1b9df453d619c991bd1cba2e2
Jared Lyon <jaredalyon@gmail.com>
2021-11-01 19:57:12 -0400

    admin bank balance commands

---
+++ moderation/addmoney.js
@@ -0,0 +1,20 @@
+module.exports = {
+    name: "addmoney",
+    permission: 2,
+    main: async function(bot, msg) {
+        const Discord = require('discord.js');
+        var recipient = msg.mentions.users.array()[0];
+        var amt = Number(msg.content.split(' ').splice(1)[0]);
+        let account = await bot.bank.get(msg.author.id);
+        var log = msg.guild.channels.cache.get(bot.config.logChannel);
+
+        if (!recipient || !amt || isNaN(amt) || amt < 0) {
+            msg.reply("something went wrong!\nUsage: `!addmoney <@user> <amt>`");
+        } else {
+            account.bank += amt;
+            log.send(msg.author.toString() + " has added $" + amt + " to " + recipient.toString() + "'s bank account.");
+            await bot.bank.update(account);
+            msg.reply("success! Log generated.");
+        }
+    }
+}

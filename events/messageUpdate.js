a25c19d1d8569ba60410319d2342bfac32e77344
Jared Lyon <jaredalyon@gmail.com>
2021-11-25 17:43:52 -0500

    Update messageUpdate.js

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -1,19 +1,17 @@
 var Discord = require('discord.js');
 
-exports.run = (messageUpdate, ) => {
-    if (!messageUpdate.author.bot) {
-        var msgUpdate = new Discord.MessageEmbed()
-            .setAuthor(messageUpdate.author.tag, messageUpdate.author.avatarURL())
-            .setTimestamp()
-            .addField('Old message:', `${oldMessage.content || "(no content)"}`)
-            .addField('New message:', `${newMessage.content || "(no content)"}`)
-            .addField(`Channel:`, `${messageUpdate.channel}`)
-            .setColor(3447003);
+exports.run = (bot, oldMessage, newMessage) => {
+    if (oldMessage === newMessage) return;
 
-            messageUpdate.guild.channels.cache.get(bot.config.messageUpdateLog).send({
-            embed: msgDelete
-        })
-    } else {
-        return;
-    }
-}
+    var msgUpdate = new Discord.MessageEmbed()
+        .setAuthor(newMessage.author.tag, newMessage.author.avatarURL())
+        .setTimestamp()
+        .addField('Old message:', `${oldMessage}`)
+        .addField('New message:', `${newMessage}`)
+        .addField(`Channel:`, `${oldMessage.channel}`)
+        .setColor(3447003);
+
+    newMessage.guild.channels.cache.get(bot.config.logChannel).send({
+        embed: msgUpdate
+    })
+};

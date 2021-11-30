7d38a62aa6b78d6e718dbe5673535cac337418ec
Jared Lyon <jaredalyon@gmail.com>
2021-11-30 18:00:38 -0500

    unmute typo fix

--- moderation/unmute.js
+++ moderation/unmute.js
@@ -23,7 +23,7 @@ module.exports = {
             
             var dm = new Discord.MessageEmbed()
                 .setAuthor(msg.guild.name, msg.guild.iconURL())
-                .setTitle(`**A moderator has issued you a warning.**`)
+                .setTitle(`**A moderator has unmuted you.**`)
                 .addField('Reason:', reason)
                 .setFooter(bot.user.username, bot.user.avatarURL())
                 .setTimestamp()

69416a1246e82963a577117a3b5b5f18849c1249
Jared Lyon <jaredalyon@gmail.com>
2021-12-03 16:29:18 -0500

    condense event + bug fixes

--- events/guildMemberUpdate.js
+++ events/guildMemberUpdate.js
@@ -1,65 +1,35 @@
 var Discord = require('discord.js');
 
 exports.run = (bot, oldMember, newMember) => {
-    if (oldMember.roles.cache.size > newMember.roles.cache.size) {
-        //role remove
+    if (oldMember.roles.cache != newMember.roles.cache) {
+        //role change
         const roleUpdate = new Discord.MessageEmbed()
             .setAuthor(newMember.user.tag, newMember.user.avatarURL())
             .setTimestamp()
             .setColor(3447003);
-
-            oldMember.roles.cache.forEach(role => {
-                if (!newMember.roles.cache.has(role.id)) {
-                    roleUpdate.addField(`Role removed:`, role);
-                }
-            });
-
-        newMember.guild.channels.cache.get(bot.config.logChannel).send({
-            embed: roleUpdate
-        })
-    } else if (oldMember.roles.cache.size < newMember.roles.cache.size) {
-        //role add
-        const roleUpdate = new Discord.MessageEmbed()
-            .setAuthor(newMember.user.tag, newMember.user.avatarURL())
-            .setTimestamp()
-            .setColor(3447003);
-
-            newMember.roles.cache.forEach(role => {
-                if (!oldMember.roles.cache.has(role.id)) {
-                    roleUpdate.addField(`Role added:`, role);
-                }
-            });
-
-        newMember.guild.channels.cache.get(bot.config.logChannel).send({
-            embed: roleUpdate
-        })
-    } else if (oldMember.roles.cache != newMember.roles.cache) {
-        //roley poley catch
-        const roleUpdate = new Discord.MessageEmbed()
-            .setAuthor(newMember.user.tag, newMember.user.avatarURL())
-            .setTimestamp()
-            .setColor(3447003);
-
-            //removed roles
-            newMember.roles.cache.forEach(role => {
-                if (!oldMember.roles.cache.has(role.id)) {
-                    roleUpdate.addField(`Role removed:`, role);
-                }
-            });
 
             //added roles
+            newMember.roles.cache.forEach(role => {
+                if (!oldMember.roles.cache.has(role.id)) {
+                    roleUpdate.addField(`Role added:`, role);
+                }
+            });
+
+            //removedroles
             oldMember.roles.cache.forEach(role => {
                 if (!newMember.roles.cache.has(role.id)) {
-                    roleUpdate.addField(`Role added:`, role);
+                    roleUpdate.addField(`Role removed:`, role);
                 }
             });
 
         newMember.guild.channels.cache.get(bot.config.logChannel).send({
             embed: roleUpdate
         })
-    } else if (oldMember.nickname != newMember.nickname) {
+    }
+    
+    if (oldMember.nickname != newMember.nickname) {
         //nickname change
-        const nicknameUpdate = new Discord.MessageEmbed()
+        var nicknameUpdate = new Discord.MessageEmbed()
             .setAuthor(newMember.user.tag, newMember.user.avatarURL())
             .setTimestamp()
             .addField('Old nickname:', `${oldMember.nickname}`)

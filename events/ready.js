991a98312b6748813cd7abe2c1712ec41112412b
Jared Lyon <jaredalyon@gmail.com>
2022-01-02 16:50:05 -0800

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -2,11 +2,8 @@ exports.run = async (bot, msg) => {
 	bot.config = require('../config.json');
 	bot.awaitConsoleInput();
 	await bot.startDatabase();
-	bot.setupList();
-	bot.setupCases();
 
 	bot.user.setPresence({ status: 'online', activity: { name: 'politics | !help', type: 0 } });
-	//bot.user.setPresence({ status: 'online', activity: { name: "It's not delivery, it's DiBjorno", type: 0 } });
 
 	bot.log(`${bot.user.username} is online and ready to serve in ${bot.channels.cache.size} channels on ${bot.guilds.cache.size} servers!`);
 }

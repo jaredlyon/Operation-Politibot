ff78b8f4895eb903f3b4c34b141ab41893ce35c0
Jared Lyon <jaredalyon@gmail.com>
2022-02-04 15:49:15 -0500

    absent user patch

--- moderation/logs.js
+++ moderation/logs.js
@@ -7,23 +7,26 @@ module.exports = {
         var caseCount = bot.caseNum.count;
         var userID = msg.content.split(' ').splice(0)[0];
         const target = msg.guild.members.cache.get(userID);
+        if (!target) {
+            msg.reply("user is no longer in the server.");
+        } else {
+            var log = new Discord.MessageEmbed()
+                .setTitle('Moderation Log History')
+                .setAuthor(target.user.username + "#" + target.user.discriminator, target.user.avatarURL())
+                .setFooter('User ID: ' + target.id)
+                .setTimestamp()
+                .setColor(3447003);
 
-        var log = new Discord.MessageEmbed()
-            .setTitle('Moderation Log History')
-            .setAuthor(target.user.username + "#" + target.user.discriminator, target.user.avatarURL())
-            .setFooter('User ID: ' + target.id)
-            .setTimestamp()
-            .setColor(3447003);
-
-        for (let i = 0; i < caseCount; i++) {
-            if (bot.logs[i] && bot.logs[i].userid == userID) {
-                var moderator = msg.guild.members.cache.get(bot.logs[i].moderatorid);
-                log.addField(bot.logs[i].type + ' issued by ' + moderator.user.username, bot.logs[i].date + '\n' + bot.logs[i].reason + '\nCase ID: ' + bot.logs[i].caseNum)
+            for (let i = 0; i < caseCount; i++) {
+                if (bot.logs[i] && bot.logs[i].userid == userID) {
+                    var moderator = msg.guild.members.cache.get(bot.logs[i].moderatorid);
+                    log.addField(bot.logs[i].type + ' issued by ' + moderator.user.username, bot.logs[i].date + '\n' + bot.logs[i].reason + '\nCase ID: ' + bot.logs[i].caseNum)
+                }
             }
-        }
 
-        msg.channel.send({
-            embed: log
-        })
+            msg.channel.send({
+                embed: log
+            })
+        }
     }
 }

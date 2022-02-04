917247e44ca1f4c42b6211837cf5662b9c0cab25
Jared Lyon <jaredalyon@gmail.com>
2022-02-03 21:23:10 -0500

    add timed mute

--- moderation/mute.js
+++ moderation/mute.js
@@ -1,4 +1,3 @@
-//849498583102914581
 var Discord = require('discord.js');
 
 module.exports = {
@@ -7,53 +6,106 @@ module.exports = {
     main: async function (bot, msg) {
         var log = msg.guild.channels.cache.get(bot.config.logChannel);
         const mutee = msg.mentions.users.first();
-        var reason = msg.content.split(' ').splice(1).join(' ');
+        var reason = msg.content.split(' ').splice(2).join(' ');
+        var length = Number(msg.content.split(' ').splice(1)[0]);
         var caseCount = bot.caseNum.count;
         if (reason === '') {
             reason = 'No reason was specified.'
         };
 
         if (mutee != null) {
-            var mute = new Discord.MessageEmbed()
+            if (!length) {
+                var mute = new Discord.MessageEmbed()
+                    .setAuthor(mutee.username, mutee.avatarURL())
+                    .addField('Member muted:', `**:mute: ${mutee} (${mutee.id}).**`)
+                    .addField('Reason:', reason)
+                    .setFooter(bot.user.username, bot.user.avatarURL())
+                    .setTimestamp()
+                    .setColor(3447003);
+
+                //await msg.guild.members.ban(mutee);
+                var dm = new Discord.MessageEmbed()
+                    .setAuthor(msg.guild.name, msg.guild.iconURL())
+                    .setTitle(`**A moderator has muted you.**`)
+                    .addField('Reason:', reason)
+                    .setFooter(bot.user.username, bot.user.avatarURL())
+                    .setTimestamp()
+                    .setColor(3447003);
+
+                let role = msg.guild.roles.cache.get("849498583102914581");
+                let member = msg.mentions.members.first();
+                member.roles.add(role);
+                await msg.channel.send({
+                    embed: mute
+                })
+                await log.send({
+                    embed: mute
+                })
+
+                bot.logs[caseCount] = {
+                    caseNum: caseCount,
+                    userid: mutee.id,
+                    moderatorid: msg.author.id,
+                    date: new Date(),
+                    type: "Mute",
+                    reason: reason
+                }
+
+                bot.caseNum.count++;
+
+                await mutee.createDM()
+                await mutee.send({
+                    emded: dm
+                })
+            } else if (length) {
+                var mute = new Discord.MessageEmbed()
                 .setAuthor(mutee.username, mutee.avatarURL())
-                .addField('Member muted:', `**:mute: ${mutee} (${mutee.id}).**`)
+                .addField(`Member muted for ${length}m:`, `**:mute: ${mutee} (${mutee.id}).**`)
                 .addField('Reason:', reason)
                 .setFooter(bot.user.username, bot.user.avatarURL())
                 .setTimestamp()
                 .setColor(3447003);
 
-            //await msg.guild.members.ban(mutee);
-            var dm = new Discord.MessageEmbed()
-                .setAuthor(msg.guild.name, msg.guild.iconURL())
-                .setTitle(`**A moderator has muted you.**`)
-                .addField('Reason:', reason)
-                .setFooter(bot.user.username, bot.user.avatarURL())
-                .setTimestamp()
-                .setColor(3447003);
+                //await msg.guild.members.ban(mutee);
+                var dm = new Discord.MessageEmbed()
+                    .setAuthor(msg.guild.name, msg.guild.iconURL())
+                    .setTitle(`**A moderator has muted you.**`)
+                    .addField('Reason:', reason)
+                    .setFooter(bot.user.username, bot.user.avatarURL())
+                    .setTimestamp()
+                    .setColor(3447003);
 
-            await mutee.createDM();
-            await mutee.send({
-                embed: dm
-            })
-            let role = msg.guild.roles.cache.get("849498583102914581");
-            let member = msg.mentions.members.first();
-            member.roles.add(role);
-            await msg.channel.send({
-                embed: mute
-            })
-            await log.send({
-                embed: mute
-            })
+                let role = msg.guild.roles.cache.get("849498583102914581");
+                let member = msg.mentions.members.first();
+                member.roles.add(role);
 
-            bot.logs[caseCount] = {
-                userid: mutee.id,
-                moderatorid: msg.author.id,
-                date: new Date(),
-                type: "Mute",
-                reason: reason
+                setTimeout(function() {
+                    member.roles.remove(role);
+                }, length * 60000);
+
+                await msg.channel.send({
+                    embed: mute
+                })
+                await log.send({
+                    embed: mute
+                })
+
+                bot.logs[caseCount] = {
+                    caseNum: caseCount,
+                    userid: mutee.id,
+                    moderatorid: msg.author.id,
+                    date: new Date(),
+                    type: "Mute",
+                    reason: reason
+                }
+
+                bot.caseNum.count++;
+
+                await mutee.createDM()
+                await mutee.send({
+                    emded: dm
+                })
             }
-
-            bot.caseNum.count++;
         } else {
             msg.reply("mention someone!")
         }

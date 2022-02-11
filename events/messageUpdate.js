223be2ef314f4a73e954531e6870b8d974f3bda0
Jared Lyon <jaredalyon@gmail.com>
2022-02-11 18:28:55 -0500

    catch errors for long message dels/edits

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -15,5 +15,22 @@ exports.run = (bot, oldMessage, newMessage) => {
 
     newMessage.guild.channels.cache.get(bot.config.logChannel).send({
         embed: msgUpdate
-    })
+    }).catch(async err => {
+        console.log(err);
+        var msgUpdate = new Discord.MessageEmbed()
+            .setAuthor(newMessage.author.tag, newMessage.author.avatarURL())
+            .setFooter(`id: ` + newMessage.author.id)
+            .setTimestamp()
+            .addField('Message edited:', `The message content was too large to be logged in an embed, so I'm forwarding it below.`)
+            .addField(`Channel:`, `${oldMessage.channel}`)
+            .setColor("#206694");
+        
+        newMessage.guild.channels.cache.get(bot.config.logChannel).send({
+            embed: msgUpdate
+        });
+        newMessage.guild.channels.cache.get(bot.config.logChannel).send("**Old Message:**");
+        newMessage.guild.channels.cache.get(bot.config.logChannel).send(oldMessage.content);
+        newMessage.guild.channels.cache.get(bot.config.logChannel).send("**New Message:**");
+        newMessage.guild.channels.cache.get(bot.config.logChannel).send(newMessage.content);
+    });
 };

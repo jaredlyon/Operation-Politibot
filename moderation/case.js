4e34a29c466788045baf46df53ef43a2314da1f4
Jared Lyon <jaredalyon@gmail.com>
2022-02-25 15:45:08 -0500

    case.js fix for #19

--- moderation/case.js
+++ moderation/case.js
@@ -12,17 +12,24 @@ module.exports = {
             msg.reply("Usage: \n`!case <number> <edit/delete> <input>` to edit\n`!case <number>` to view\nError: 1");
         } else if (bot.logs[caseNumber]) {
             if (!input) {
-                var userID = bot.logs[caseNumber].userid;
-                const target = msg.guild.members.cache.get(userID);
+                var userid = bot.logs[caseNumber].userid;
                 var moderator = msg.guild.members.cache.get(bot.logs[caseNumber].moderatorid);
 
                 var log = new Discord.MessageEmbed()
                     .setTitle('Case ' + bot.logs[caseNumber].caseNum)
-                    .setAuthor(target.user.username + "#" + target.user.discriminator, target.user.avatarURL())
                     .addField(bot.logs[caseNumber].type + ' issued by ' + moderator.user.username, bot.logs[caseNumber].date + '\n' + bot.logs[caseNumber].reason)
-                    .setFooter('User ID: ' + target.id)
                     .setTimestamp()
                     .setColor(3447003);
+
+                    if (msg.guild.members.cache.get(userid)) {
+                        var target = msg.guild.members.cache.get(bot.logs[caseNumber].userid);
+                        log.setAuthor(target.user.username + "#" + target.user.discriminator, target.user.avatarURL());
+                        log.setFooter('User ID: ' + target.id)
+                    } else {
+                        var target = bot.logs[caseNumber].userid;
+                        log.setAuthor("(User left server) ID: " + target);
+                        log.setFooter('User ID: ' + userid);
+                    }
     
     
                 msg.channel.send({

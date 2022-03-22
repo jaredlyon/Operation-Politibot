d6624e276907bbbd8b30adafe59040b95dbdd8ee
Jared Lyon <jaredalyon@gmail.com>
2022-03-21 23:52:59 -0400

    fix userinfo inputs

--- moderation/userinfo.js
+++ moderation/userinfo.js
@@ -4,11 +4,16 @@ module.exports = {
     name: "userinfo",
     permission: 1,
     main: function (bot, msg) {
+        var input = msg.content.split(' ').splice(0)[0];
+
         if (msg.mentions.users.first()) {
             var target = msg.mentions.members.first();
         } else if (!msg.mentions.users.first()) {
-            var userID = msg.content.split(' ').splice(0)[0];
-            var target = msg.guild.members.cache.get(userID);
+            if (input === "!userinfo") {
+                var target = msg.member;
+            } else {
+                var target = msg.guild.members.cache.get(input);
+            }
         }
 
         var info = new Discord.MessageEmbed()

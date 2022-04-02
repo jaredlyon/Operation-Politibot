540a441dcd0291c1fe5100dbaeebde269c103512
Jared Lyon <jaredalyon@gmail.com>
2022-04-02 18:32:55 -0400

    add member counts to joins/leaves

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -4,6 +4,7 @@ exports.run = (bot, member) => {
     var channel = member.guild.channels.cache.get(bot.config.logChannel);
     var leave = new Discord.MessageEmbed()
         .setAuthor(member.user.username, member.user.avatarURL())
+        .addField('Member Count:', member.guild.memberCount)
         .setFooter(`id: ` + member.user.id)
         .setTimestamp()
         .setTitle('Member left!')

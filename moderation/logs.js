293108adc84798d027262ac1760ed8cd81530cf4
Jared Lyon <jaredalyon@gmail.com>
2022-04-13 19:12:48 -0400

    revert date thingie + update msg filter

--- moderation/logs.js
+++ moderation/logs.js
@@ -30,12 +30,8 @@ module.exports = {
 
         for (let i = 0; i < caseCount; i++) {
             if (bot.logs[i] && bot.logs[i].userid == userid) {
-                if (msg.guild.members.cache.get(bot.logs[i].userid)) {
-                    var modUsername = msg.guild.members.cache.get(bot.logs[i].moderatorid).user.username;
-                } else {
-                    var modUsername = bot.logs[i].moderatorid;
-                }
-                log.addField(bot.logs[i].type + ' issued by ' + modUsername, bot.logs[i].date.substring(0, 10) + '\n' + bot.logs[i].reason + '\nCase ID: ' + bot.logs[i].caseNum)
+                var moderator = msg.guild.members.cache.get(bot.logs[i].moderatorid);
+                log.addField(bot.logs[i].type + ' issued by ' + moderator.user.username, bot.logs[i].date + '\n' + bot.logs[i].reason + '\nCase ID: ' + bot.logs[i].caseNum)
             }
         }
 

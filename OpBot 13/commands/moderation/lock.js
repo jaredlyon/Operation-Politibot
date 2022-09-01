174c077e642df6cf50f3d230889bef821f2c62f7
raylou15 <86377551+raylou15@users.noreply.github.com>
2022-09-01 02:14:23 -0400

    Update Lock.js to Slash Command

--- OpBot 13/commands/moderation/lock.js
+++ OpBot 13/commands/moderation/lock.js
@@ -2,18 +2,35 @@ const { Client, Message, MessageEmbed, MessageButton } = require('discord.js');
 const { MessageActionRow, MessageSelectMenu } = require('discord.js');
 
 module.exports = {
-	name: "lock",
-    description: "Locks the current channel",
-    options: [],
-	run: async(client, interaction) => {
-		//channel to lock down
-        let channels = interaction.guild.channels;
-        let targetChannelID = interaction.channel.id;
-        let targetChannel = channels.cache.find(r => r.id === targetChannelID);
+    name: "lock",
+    category: "Moderation",
+    description: "Lock a channel to stop those hooligans!",
+    options: [
+        {
+            type: 7,
+            name: "channel",
+            description: "The channel you wish to lock",
+        },
+    ],
+
+    run: async(client, interaction) => {
+
+        // const targetChannel = interaction.options.getChannel('channel');
+
+        if (interaction.options.getChannel('channel') == null) {
+            targetChannel = interaction.channel;
+        } else {
+            targetChannel = interaction.options.getChannel('channel');
+        };
 
         //channel lockdown
-        await targetChannel.updateOverwrite(interaction.guild.id, { SEND_MESSAGES: false });
+        await targetChannel.permissionOverwrites.edit(interaction.guild.id, { SEND_MESSAGES: false });
 
-        interaction.reply('channel locked! `!unlock` to unlock.')
-	},
-};
+        interaction.reply({
+            content: 'Channel locked! `/unlock` to unlock the channel.',
+            ephemeral: true,
+        })
+        targetChannel.send("This channel has been locked by a moderator.")
+    }
+
+}

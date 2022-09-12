161e296f9eba5dd04799649a97c9ae7348c4ea70
raylou15 <86377551+raylou15@users.noreply.github.com>
2022-09-11 21:10:35 -0400

    update to slash with fancy modals

--- OpBot 13/commands/moderation/suggest.js
+++ OpBot 13/commands/moderation/suggest.js
@@ -1,138 +1,230 @@
-const { Client, Message, MessageEmbed, MessageButton } = require('discord.js');
-const { MessageActionRow, MessageSelectMenu } = require('discord.js');
+const { Client, Message, MessageEmbed, MessageButton, ButtonStyle, MessageActionRow, MessageSelectMenu } = require('discord.js');
+const { Modal, TextInputComponent, showModal, ModalSubmitInteraction } = require('discord-modals');
 
 module.exports = {
-	name: "suggest",
+    name: "suggest",
+    category: "Utility",
     description: "Send a suggestion to server staff",
-    options: [],
-	run: async(client, interaction) => {
-		await interaction.reply('This message is still being updated to Discord.js v13, sorry!');
+    options: [
+        {
+            type: 3,
+            name: "suggestion",
+            description: "Write your suggestion here!",
+            required: true
+        }
+    ],
+    /**
+     * 
+     * @param {Modal} modal
+     */
+    run: async (client, interaction) => {
 
-        /*
-         const yup = bot.emojis.cache.find(emoji => emoji.name == "tickmark").toString();
-        const nope = bot.emojis.cache.find(emoji => emoji.name == "crossmark").toString();
-        const up = bot.emojis.cache.find(emoji => emoji.name == "upvote").toString();
-        const down = bot.emojis.cache.find(emoji => emoji.name == "downvote").toString();
-        const neut = bot.emojis.cache.find(emoji => emoji.name == "neutralvote").toString();
-        var log = msg.guild.channels.cache.get(bot.config.logChannel);
-        const args = msg.content.slice(0).trim().split(/ +/g);
-        var reason = args.splice(0).join(' ');
-        if (reason === '') {
-            reason = 'No text was specified.'
+        // Just grabbing the user for something later...
+        const intAuth = interaction.user;
+
+        // Setting up the button menus for the interaction...
+        const submitMenu = new MessageActionRow()
+            .addComponents(
+                new MessageButton()
+                    .setCustomId('submit')
+                    .setLabel('Submit')
+                    .setStyle('SUCCESS'),
+                new MessageButton()
+                    .setCustomId('cancel')
+                    .setLabel('Cancel')
+                    .setStyle('DANGER'),
+            );
+
+        const reviewMenu = new MessageActionRow()
+            .addComponents(
+                new MessageButton()
+                    .setCustomId('approve')
+                    .setLabel('Approve')
+                    .setStyle('SUCCESS'),
+                new MessageButton()
+                    .setCustomId('reject')
+                    .setLabel('Reject')
+                    .setStyle('DANGER'),
+            );
+
+        // Voting Emojis...
+        const up = client.emojis.cache.find(emoji => emoji.name == "upvote").toString();
+        const down = client.emojis.cache.find(emoji => emoji.name == "downvote").toString();
+        const neut = client.emojis.cache.find(emoji => emoji.name == "neutralvote").toString();
+
+        // Channels and stuff
+        var log = interaction.guild.channels.cache.get(client.config.logChannel);
+        var sendChannel = interaction.guild.channels.cache.get('965271666684985454')
+        var rayChannel = interaction.guild.channels.cache.get('775494762216161341')
+        // var staffChannel = interaction.guild.channels.cache.get('893189722887839797')
+
+        // Getting the string from the input
+        const suggestionText = interaction.options.getString('suggestion');
+
+        // Message Embeds and stuff
+        const submitEmbed = {
+            color: '#ffffff',
+            author: {
+                name: 'Suggestion from ' + interaction.user.username,
+                icon_url: interaction.user.avatarURL(),
+            },
+            description: suggestionText + "\n\n*Do you want to submit this suggestion?*",
+            timestamp: new Date()
         };
 
-        //suggestions channel: 965271666684985454
-        var sendChannel = msg.guild.channels.cache.get('965271666684985454');
-        //ray channel: 775494762216161341
-        var rayChannel = msg.guild.channels.cache.get('775494762216161341');
-        //staff channel: 893189722887839797
-        var staffChannel = msg.guild.channels.cache.get('893189722887839797');
+        const reviewEmbed = {
+            color: '#ffffff',
+            author: {
+                name: 'Suggestion from ' + interaction.user.username,
+                icon_url: interaction.user.avatarURL(),
+            },
+            description: suggestionText + "\n\n*Do you want to approve this suggestion?*",
+            timestamp: new Date()
+        };
 
-        let initEmbed = await msg.channel.send({
-            embed: {
-                color: 0x33cc33,
-                author: {
-                    name: 'Suggestion from ' + msg.author.username,
-                    icon_url: msg.author.avatarURL(),
-                },
-                description: "*" + reason + "*\n" + yup + " to submit / " + nope + " to delete",
-                timestamp: new Date()
-            }
-        }).then(async function (message) {
-            await message.react(yup)
-            await message.react(nope)
-            return message;
+        const suggestEmbed = {
+            color: '#ffffff',
+            author: {
+                name: 'Suggestion from ' + interaction.user.username,
+                icon_url: interaction.user.avatarURL(),
+            },
+            description: suggestionText + "\n\n*" + up + " to upvote / " + neut + " to abstain / " + down + " to downvote*",
+            timestamp: new Date()
+        };
+
+        const suggestEmbedPreview = {
+            color: '#ffffff',
+            author: {
+                name: 'Suggestion from ' + interaction.user.username,
+                icon_url: interaction.user.avatarURL(),
+            },
+            description: suggestionText,
+            timestamp: new Date()
+        }
+
+        // Sending a confirmation message to submit the suggestion for review
+        const msgA = await interaction.reply({
+            ephemeral: true,
+            embeds: [submitEmbed],
+            components: [submitMenu],
+            fetchReply: true,
         });
 
-        let initFilter = (reaction, user) =>
-        (reaction.emoji.name === 'tickmark' || reaction.emoji.name === 'crossmark') &&
-        user.id === msg.author.id;
-  
-        let initCollector = initEmbed.createReactionCollector(initFilter, {
-            time: 1000 * 60 * 3
-        });
+        // Just a fancy filter majig
+        const filter = i => {
+            return i.user.id === interaction.user.id;
+        };
 
-        var initTimeout = true;
-    
-        initCollector.on("collect", async messageReaction => {
-            if (messageReaction.emoji.name === 'tickmark') {
-                msg.reply('your suggestion has been submitted for review.')
-                //await staffChannel.send("<@178689418415177729>")
-                let rayEmbed = await staffChannel.send({
-                    embed: {
-                        color: 0x33cc33,
-                        author: {
-                            name: 'Suggestion from ' + msg.author.username,
-                            icon_url: msg.author.avatarURL(),
-                        },
-                        description: "*" + reason + "*\n\n" + yup + " to approve / " + nope + " to deny",
-                        timestamp: new Date()
-                    }
-                }).then(async function (message) {
-                    await message.react(yup)
-                    await message.react(nope)
-                    return message;
-                });
+        // The actual code
+        msgA.awaitMessageComponent( filter ).then(async interaction => {
 
-                let nextFilter = (reaction, user) =>
-                (reaction.emoji.name === 'crossmark' || reaction.emoji.name === 'tickmark') &&
-                (user.id === '178689418415177729' || user.id === '233325738013491200' || user.id === '133350262420013056' || user.id === '202165686607282176' || user.id === '213534403459022848');
-          
-                let nextCollector = rayEmbed.createReactionCollector(nextFilter, {
-                    time: 1000 * 24 * 60 * 60
-                });
+            buttonClickValue = interaction.customId
 
-                var nextTimeout = true;
+            if (buttonClickValue === 'cancel') {
+                await interaction.update({
+                    content: '*Suggestion cancelled.*',
+                    ephemeral: true,
+                    embeds: [],
+                    components: [],
+                })
+            } else if (buttonClickValue === 'submit') {
+                await interaction.update({
+                    content: '*Your suggestion has been submitted for review by server management! You will receive a DM shortly.*',
+                    ephemeral: true,
+                    embeds: [],
+                    components: [],
+                })
 
-                nextCollector.on("collect", async messageReaction => {
-                    if (messageReaction.emoji.name === 'tickmark') {
-                        msg.reply('your earlier suggestion was approved and will be shown in #suggestions.')
-                        await sendChannel.send({
-                            embed: {
-                                color: 0x33cc33,
-                                author: {
-                                    name: 'Suggestion from ' + msg.author.username,
-                                    icon_url: msg.author.avatarURL(),
-                                },
-                                description: "*" + reason + "*\n\n" + up + " to upvote / " + neut + " to abstain / " + down + " to downvote",
-                                timestamp: new Date()
-                            }
+                const confirmMsg = await rayChannel.send({
+                    content: '<@178689418415177729>, a new suggestion needs review!',
+                    embeds: [reviewEmbed],
+                    components: [reviewMenu],
+                    fetchReply: true,
+                }) 
+
+                confirmMsg.awaitMessageComponent( filter ).then(async interaction => {
+
+                    buttonClickValue2 = interaction.customId
+
+                    // If it's confirmed by the Management...
+                    if (buttonClickValue2 === 'approve') {
+                        await confirmMsg.edit({
+                            content: 'Suggestion has been **approved**. Check <#965271666684985454>!',
+                            embeds: [suggestEmbedPreview],
+                            components: [],
+                        })
+                        // Send message to #suggestions channel!
+                        sendChannel.send({
+                            embeds: [suggestEmbed],
                         }).then(async function (message) {
                             await message.react(up)
                             await message.react(neut)
                             await message.react(down)
                             return message;
                         });
+                        // DM the user a confirmation here!
+                        await intAuth.send({
+                            content: 'Your suggestion has been approved and submitted to <#965271666684985454> for voting! \n\nApproved Suggestion:',
+                            embeds: [suggestEmbedPreview],
+                        })
+                    }
 
-                        nextTimeout = false;
-                    } else if (messageReaction.emoji.name === 'crossmark') {
-                        msg.reply('your earlier suggestion was rejected and will not be shown in #suggestions.')
+                    // If it's rejected by the Management...
+                    if (buttonClickValue2 === 'reject') {
+                        const reasonModal = new Modal()
+                            .setTitle('Rejection Reason')
+                            .setCustomId('reject_reason');
+                        
+                        const reasonInputComponent = new TextInputComponent()
+                            .setCustomId('reasoninput')
+                            .setLabel('Reason:')
+                            .setMinLength(2)
+                            .setMaxLength(1000)
+                            .setRequired(false)
+                            .setStyle("PARAGRAPH")
+                            .setPlaceholder("Enter a reason here");
 
-                        nextTimeout = false;
-                        nextCollector.stop();
+                        const row_Input = new MessageActionRow().addComponents(reasonInputComponent);
+
+                        reasonModal.addComponents(reasonInputComponent);
+
+                        await interaction.showModal(reasonModal);
+
+                        await interaction.awaitModalSubmit({ time: 60000, }).then(async interaction => {
+                            const reasonInputted = await interaction.fields.getTextInputValue('reasoninput')
+                            if (reasonInputted === null) {
+                                reasonInputted = "No reason was given."
+                            }
+
+                            const reasonEmbed = {
+                                color: '#ffffff',
+                                author: {
+                                    name: 'Suggestion from ' + intAuth.username + ' rejected by ' + interaction.user.username,
+                                    icon_url: interaction.user.avatarURL(),
+                                },
+                                description: "**Suggestion:** \n" + suggestionText + "**\n\nReason: \n**" + reasonInputted,
+                                timestamp: new Date()
+                            };
+
+                            await interaction.reply({
+                                embeds: [reasonEmbed],
+                                components: [],
+                            });
+
+                            await confirmMsg.edit({
+                                embeds: [suggestEmbedPreview],
+                                components: [],
+                            });
+
+                            await intAuth.send({
+                                content: 'Your suggestion has been **rejected** for the following reasons:',
+                                embeds: [reasonEmbed],
+                            });
+
+                        })
                     }
                 });
-
-                nextCollector.on("end", async collected => {
-                    if (nextTimeout) {
-                        rayChannel.send('Above suggestion timed out.')
-                    }
-                });
-
-                initTimeout = false;
-            } else if (messageReaction.emoji.name === 'crossmark') {
-                msg.reply("suggestion deleted!")
-
-                initTimeout = false;
-                initCollector.stop();
             }
-        });
-
-        initCollector.on("end", async collected => {
-            if (initTimeout) {
-                msg.reply('your suggestion timed out.')
-            }
-        });
-        */
-	},
+        })
+    },
 };

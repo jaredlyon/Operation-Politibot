ac439afb537b3ebb982f483d9fcec017eecbd586
Jared Lyon <jaredalyon@gmail.com>
2022-09-17 13:21:45 -0400

    fix approve

--- OpBot 13/commands/moderation/approve.js
+++ OpBot 13/commands/moderation/approve.js
@@ -6,51 +6,51 @@ module.exports = {
     description: "Approves a user for member status",
     options: [
       {
-        "type": 6,
-        "name": "target",
-        "description": "The user to be approved",
-        "required": true
+        type: 6,
+        name: "target",
+        description: "The user to be approved",
+        required: true
       }
     ],
 	run: async(client, interaction) => {
         const tick = client.emojis.cache.find(emoji => emoji.name == "tickmark").toString();
-        var channel = interaction.guild.channels.cache.get(client.config.welcomeChannel); //mod channel
         var log = interaction.guild.channels.cache.get(client.config.logChannel);  //logs the stuff
-        var target = interaction.options.get('target');
+
+        const targetUser = await interaction.options.getUser('target');
+        const userID = targetUser.id;
+        const targetMember = await interaction.guild.members.fetch(targetUser);
         
         //member id: 909989200378601472
         //untrusted id: 909988798308433920
 
         if (interaction.channel.id == client.config.welcomeChannel) {
-            if (target != null) {
-                if (!target.roles.cache.some(role => role.id === '909989200378601472') && target.roles.cache.some(role => role.id === '909988798308433920')) {
-                    var logEmbed = new MessageEmbed()
-                        .setAuthor(interaction.author.username, interaction.author.avatarURL())
-                        .addField('Member approved:', tick + ` **${target} (${target.id}) was approved.**`)
-                        .setFooter(client.user.username, client.user.avatarURL())
-                        .setTimestamp()
-                        .setColor("#FFFFFF");
+            if (!targetMember.roles.cache.some(role => role.id === '909989200378601472') && targetMember.roles.cache.some(role => role.id === '909988798308433920')) {
+                var logEmbed = new MessageEmbed()
+                    .setAuthor(interaction.user.username, interaction.user.avatarURL())
+                    .addField('Member approved:', tick + ` **${targetUser.username} (${targetUser.id}) was approved.**`)
+                    .setFooter(client.user.username, client.user.avatarURL())
+                    .setTimestamp()
+                    .setColor("#FFFFFF");
 
-                    let trusted = (await client.trusted.get(userID)) || {};
+                let trusted = (await client.trusted.get(userID)) || {};
 
-                    await target.roles.add('909989200378601472');
-                    await target.roles.remove('909988798308433920');
-                    trusted.joinDate = new Date();
-                    await client.trusted.update(trusted);
-                    await channel.send({
-                        embeds: [logEmbed]
-                    })
-                    await log.send({
-                        embeds: [logEmbed]
-                    })
-                } else {
-                    interaction.reply('this user has either already been approved or has not accepted our rules prompt.');
-                }
+                await targetMember.roles.add('909989200378601472');
+                await targetMember.roles.remove('909988798308433920');
+
+                trusted.joinDate = new Date();
+                await client.trusted.update(trusted);
+                
+                await interaction.reply({
+                    embeds: [logEmbed]
+                })
+                await log.send({
+                    embeds: [logEmbed]
+                })
             } else {
-                interaction.reply("target not found! Usage: `!approve [user ID]`\n*Be sure you've correctly copied the user's ID and you're not mentioning them!*");
+                interaction.reply('This user has either already been approved or has not accepted our rules prompt.');
             }
         } else {
-            interaction.reply("wrong channel!");
+            interaction.reply("**This command can only be used in #staff-commands.**");
         }
 	},
 };

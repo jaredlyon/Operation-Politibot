12445c7f32278f974815400e0c629f01966899c3
Jared Lyon <jaredalyon@gmail.com>
2022-09-23 11:44:54 -0400

    add repmod

--- commands/moderation/repmod.js
+++ commands/moderation/repmod.js
@@ -43,17 +43,65 @@ module.exports = {
           required: true
         }
       ]
+    },
+    {
+      type: 1,
+      name: "add",
+      description: "Grant a rep to a user!",
+      options: [
+        {
+          type: 6,
+          name: "target",
+          description: "The target user",
+          required: true
+        }
+      ]
     }
   ],
   run: async (client, interaction) => {
     if (interaction.options.getSubcommand() === "10") {
       var targetUser = interaction.options.getUser("target");
+      var targetID = targetUser.id;
+
+      client.repData[targetID].repScore += 10;
+      //client.repData[interaction.user.id].repsGiven += 10;
+      client.repData[targetID].lastRepReceived = new Date();
+      //client.repData[interaction.user.id].lastRepGiven = new Date();
+      interaction.reply(targetUser.toString() + " has been given 10 rep by " + interaction.user.toString() + "!");
+      console.log("[REP DATA] " + interaction.user.username + " has given 10 rep to " + targetUser.username + ".");
 
     } else if (interaction.options.getSubcommand() === "-10") {
       var targetUser = interaction.options.getUser("target");
+      var targetID = targetUser.id;
+
+      client.repData[targetID].repScore -= 10;
+      //client.repData[interaction.user.id].repsGiven += 10;
+      //client.repData[targetID].lastRepReceived = new Date();
+      //client.repData[interaction.user.id].lastRepGiven = new Date();
+      interaction.reply(targetUser.toString() + " has had 10 rep taken by " + interaction.user.toString() + "!");
+      console.log("[REP DATA] " + interaction.user.username + " has taken 10 rep from " + targetUser.username + ".");
 
     } else if (interaction.options.getSubcommand() === "remove") {
       var targetUser = interaction.options.getUser("target");
+      var targetID = targetUser.id;
+
+      client.repData[targetID].repScore--;
+      //client.repData[interaction.user.id].repsGiven += 10;
+      //client.repData[targetID].lastRepReceived = new Date();
+      //client.repData[interaction.user.id].lastRepGiven = new Date();
+      interaction.reply(targetUser.toString() + " has had rep stripped from them by " + interaction.user.toString() + "!");
+      console.log("[REP DATA] " + interaction.user.username + " has stripped a rep from " + targetUser.username + ".");
+
+    } else if (interaction.options.getSubcommand() === "add") {
+      var targetUser = interaction.options.getUser("target");
+      var targetID = targetUser.id;
+
+      client.repData[targetID].repScore++;
+      //client.repData[interaction.user.id].repsGiven += 10;
+      //client.repData[targetID].lastRepReceived = new Date();
+      //client.repData[interaction.user.id].lastRepGiven = new Date();
+      interaction.reply(targetUser.toString() + " has been repped by " + interaction.user.toString() + "!");
+      console.log("[REP DATA] " + interaction.user.username + " has given rep to " + targetUser.username + ".");
 
     }
   }

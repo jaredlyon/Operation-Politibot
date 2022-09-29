2cfd9c637e9f5f70595c42b0a7e0e0127acb160d
Jared Lyon <jaredalyon@gmail.com>
2022-09-29 17:13:35 -0400

    #49 add null catches

--- events/clientEvents/messageUpdate.js
+++ events/clientEvents/messageUpdate.js
@@ -6,36 +6,46 @@ module.exports = {
 		//if (new Date() - oldMessage.createdAt <= 2000 && newMessage.embeds.length > 0) return;
         if (oldMessage.content === newMessage.content) return;
 
-        if (oldMessage.content && newMessage.content) {
+        if (oldMessage.content) {
+            var oldMessageContent = oldMessage.content;
+        } else {
+            var oldMessageContent = "`null`"
+        }
+
+        if (newMessage.content) {
+            var newMessageContent = newMessage.content;
+        } else {
+            var newMessageContent = "`null`"
+        }
+
+        var msgUpdate = new MessageEmbed()
+            .setAuthor(newMessage.author.tag, newMessage.author.avatarURL())
+            .setFooter(`id: ` + newMessage.author.id)
+            .setTimestamp()
+            .addField('Old message:', `${oldMessageContent}`)
+            .addField('New message:', `${newMessageContent}`)
+            .addField(`Channel:`, `${oldMessage.channel}`)
+            .setColor("#206694");
+    
+        newMessage.guild.channels.cache.get(client.config.logChannel).send({
+            embeds: [msgUpdate]
+        }).catch(async err => {
+            console.log(err);
             var msgUpdate = new MessageEmbed()
                 .setAuthor(newMessage.author.tag, newMessage.author.avatarURL())
                 .setFooter(`id: ` + newMessage.author.id)
                 .setTimestamp()
-                .addField('Old message:', `${oldMessage}`)
-                .addField('New message:', `${newMessage}`)
+                .addField('Message edited:', `The message content was too large to be logged in an embed, so I'm forwarding it below.`)
                 .addField(`Channel:`, `${oldMessage.channel}`)
                 .setColor("#206694");
-        
+            
             newMessage.guild.channels.cache.get(client.config.logChannel).send({
                 embeds: [msgUpdate]
-            }).catch(async err => {
-                console.log(err);
-                var msgUpdate = new MessageEmbed()
-                    .setAuthor(newMessage.author.tag, newMessage.author.avatarURL())
-                    .setFooter(`id: ` + newMessage.author.id)
-                    .setTimestamp()
-                    .addField('Message edited:', `The message content was too large to be logged in an embed, so I'm forwarding it below.`)
-                    .addField(`Channel:`, `${oldMessage.channel}`)
-                    .setColor("#206694");
-                
-                newMessage.guild.channels.cache.get(client.config.logChannel).send({
-                    embeds: [msgUpdate]
-                });
-                newMessage.guild.channels.cache.get(client.config.logChannel).send("**Old Message:**");
-                newMessage.guild.channels.cache.get(client.config.logChannel).send(oldMessage.content);
-                newMessage.guild.channels.cache.get(client.config.logChannel).send("**New Message:**");
-                newMessage.guild.channels.cache.get(client.config.logChannel).send(newMessage.content);
             });
-        }
+            newMessage.guild.channels.cache.get(client.config.logChannel).send("**Old Message:**");
+            newMessage.guild.channels.cache.get(client.config.logChannel).send(oldMessage.content);
+            newMessage.guild.channels.cache.get(client.config.logChannel).send("**New Message:**");
+            newMessage.guild.channels.cache.get(client.config.logChannel).send(newMessage.content);
+        });
 	},
 };

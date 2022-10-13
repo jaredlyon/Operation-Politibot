abe3afd6b24d7a2da01bbb3e82415eb9484b2d18
raylou15 <rlourigan1@gmail.com>
2022-10-12 23:50:50 -0400

    add new citation stuff

---
+++ commands/contextmenus/citationreport.js
@@ -0,0 +1,77 @@
+const { Client, Message, MessageEmbed, MessageButton } = require('discord.js');
+const { MessageActionRow, MessageSelectMenu } = require('discord.js');
+const { Modal, TextInputComponent, showModal, ModalSubmitInteraction } = require('discord-modals');
+const { ContextMenuInteraction } = require('discord.js')
+
+module.exports = {
+    name: 'Report Citation',
+    type: 'MESSAGE',
+    /**
+     * 
+     * @param {ContextMenuInteraction} interaction 
+     */
+    run: async (client, interaction) => {
+
+        const targetMsg = await interaction.channel.messages.fetch(interaction.targetId);
+        const targetUser = await targetMsg.author
+        const notifyChannel = interaction.guild.channels.cache.get('893189759474757693')
+
+        const filter = i => {
+            return i.user.id === interaction.user.id;
+        };
+
+        const reportModal = new Modal()
+            .setTitle('Report a Citation')
+            .setCustomId('report_modal');
+
+        const appealInputField = new TextInputComponent()
+            .setCustomId('complaint')
+            .setLabel('Complaint')
+            .setMinLength(20)
+            .setMaxLength(2000)
+            .setRequired(true)
+            .setStyle('PARAGRAPH')
+            .setPlaceholder('Why are you reporting this citation? Inaccurate? Misleading? Unreliable?');
+
+        reportModal.addComponents(appealInputField)
+
+        await interaction.showModal(reportModal)
+
+        await interaction.awaitModalSubmit({ time: 120000, }).then(async interaction => {
+            const complaintInput = interaction.fields.getTextInputValue('complaint')
+
+            const notifyEmbed = {
+                //            .setAuthor(interaction.user.username, interaction.user.avatarURL())
+                author: {
+                    name: interaction.user.username,
+                    icon_url: interaction.user.avatarURL(),
+                },
+                color: '#992D22',
+                title: '**Citation Complaint Filed**',
+                description: "Against: " + targetUser.toString() + `\n\n[Jump to Message](${targetMsg.url})\n\n*Please note that this is a **citation report**. If this is a simple mistake, just use the context menu interaction (right click -> apps) to prompt the user with a reminder of how to properly cite and find reliable sources in a debate to back up their claims. Otherwise, if you think there is misconduct that can fall under Rule 11, talk with other Moderators or issue an appropriate judgement.*`,
+                fields: [
+                    {
+                        name: 'Reason:',
+                        value: complaintInput,
+                    },
+                    {
+                        name: 'Timestamp:',
+                        value: "In channel " + interaction.channel.toString() + " at: \n`" + interaction.createdAt + "`" 
+                    },
+                ],
+                footer: {
+                    text: client.user.username,
+                    icon_url: client.user.avatarURL(),
+                }
+            }
+
+            notifyChannel.send({
+                content: "<@178689418415177729> <@&893189360105689139> <@&854841000480079882> <@&927318500614225920> **See below complaint:**",
+                embeds: [notifyEmbed],
+            })
+
+            interaction.reply({ content: 'Thank you for submitting a report! It will be reviewed by Moderators shortly. Please be sure to use <#999439440273473657> when possible!\n\nBecause this is a citation report, there may be no actual action taken. We prefer these kinds of issues to work themselves out through debate and discourse, but if we can find genuine misbheavior or intent to purposefully mislead, then we will take action under Rule 11. Otherwise, you may at most see a reminder message pop up to remind someone of how to cite sources appropriately. If you have any questions, let us know!', ephemeral: true })
+
+        })
+    }
+}
--- events/clientEvents/interactionCreate.js
+++ events/clientEvents/interactionCreate.js
@@ -339,6 +339,59 @@ module.exports = {
                     })
                 })
             }
+
+            if (interaction.customId === 'moreinfopls') {
+                const moreinfoembed = {
+                    color: '#ffffff',
+                    title: '🔬  Tips and Tricks for Citing Sources',
+                    fields: [
+                        {
+                            name: "🔍  Finding Appropriate Sources",
+                            value: "Finding good sources can be a challenge. As a general rule of thumb, news articles are okay as long as the news organization is generally reliable. For medical sources, you should try to focus on things like PubMed and other Medical Journals, and stay away from op-eds and random websites. For political research, there are lots of good websites and firms that organize and deliver that kind of stuff. Check out [Google Scholar](https://scholar.google.com/) if you want help finding professional academic sources.",
+                            inline: false, 
+                        },
+                        {
+                            name: "📰  Dealing with the News",
+                            value: "Citing news sources can be an important part of political discourse because of how much of it is centered around the idea of a free press. Generally, there are quite a few reliable news organizations (regardless of your weird political opinions about them) that can often be used as good sources. \n\nThe **best** news sources actually provide citations when they're discussing or bringing up other things. However, this can easily blur the line between news and editorials, so be sure to NOT post opinion articles.\n\nA list of some good news organizations includes: CNN, New York Times, Fox News (outside their editorials), The Washington Post, **AP News, Reuters,** NPR, Wall Street Journal, BBC, Bloomberg News, Foreign Affairs, and Politico. There are others, but these are some of the best!",
+                            inline: false, 
+                        },
+                        {
+                            name: "🏛️  Government Sources",
+                            value: "Data and reports are generally fine. Government-sourced media, news, statements, and otherwise generally should not be taken as fact unless they can be corroborated by reliable free press sources.",
+                            inline: true, 
+                        },
+                        {
+                            name: "🏷️  Affiliations and Credentials",
+                            value: "Checking an author's affiliations and credentials are not always important, but making sure they are relevant to the field they are publishing about, are well-established, or aren't affiliated with biased political groups is important.",
+                            inline: true, 
+                        },
+                        {
+                            name: "🤔  Be Skeptical",
+                            value: "A lot of people will simply look at the title and the first few words in the Abstract of a research paper and say \"Well, this is what I need!\". Don't do that. If you're not familiar with the paper / resource in full, then at least read the full Abstract before posting it. Never take any source at first glance without understanding completely what it is about. A lot of publishers offer you free access to look at research papers if you are part of a college or school - you should try to make sure your paper mentions what you're looking for!",
+                            inline: false, 
+                        },
+                        {
+                            name: "📅  Make Sure It's Up to Date",
+                            value: "Publishing a study or source from 1965, unless it is the most recent source available or you are specifically discussing that for some reason, should generally be taken with a grain of salt. Try to make sure your research is as up-to-date as possible, as newer research sometimes has better methodology and refutes old research. Don't get caught lacking!",
+                            inline: true, 
+                        },
+                        {
+                            name: "🏅  Peer-Reviewed Research",
+                            value: "Always try to make sure the research your post is peer-reviewed. This means that it has undergone a level of scrutiny and verification that ensures that it was conducted properly with good methodology and generally reasonable or factuals hypotheses and assumptions and reliable, repeatable results. There are some instances where peer-reviewed research *can* be bad, but just make sure it's peer-reviewed.",
+                            inline: true, 
+                        },
+                        {
+                            name: "Still looking for more tips and tricks?",
+                            value: "This embed is already too long, and there's so much more that could be said! If you're looking for more tips and tricks on how to find reliable, reputable sources, please check out the following links below:\n[Paperpile](https://paperpile.com/g/find-credible-sources/) - *\"How can I find credible sources?\"*\n[Scribbr](https://www.scribbr.com/working-with-sources/credible-sources/) - *A brief lesson on sources, and explains the \"CRAAP\" test, a good tool!*\n[University of the People](https://www.uopeople.edu/blog/ultimate-student-guide-to-finding-credible-sources/) - *Another brief lesson, but with some more good journals and tips.*\n[UTEP](https://libguides.utep.edu/c.php?g=429972&p=2931971) - *Lots of good Political Science source websites here.*\n[Washington & Jefferson College](https://libguides.washjeff.edu/politicalscience/websites) - *Provides lots of good tips for googling.*",
+                            inline: false, 
+                        },
+                    ]
+                };
+
+                interaction.reply({ embeds: [moreinfoembed], ephemeral: true })
+
+            }
+
         }
 
         // if (interaction.isSelectMenu()) {
